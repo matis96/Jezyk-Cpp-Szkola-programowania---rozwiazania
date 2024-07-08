e2c488afc71b069e82235635e0f3b3013fcfdeec
Matis <mateuszgrzyb96@gmail.com>
2024-07-08 18:22:03 +0200

    Create pe12_4.cpp

---
+++ chapter 12/pe12_4/pe12_4.cpp
@@ -0,0 +1,84 @@
+// stacker.cpp -- test klasy Stack
+#include <iostream>
+#include <cctype>                // albo ctype.h
+#include "stack.h"
+int main()
+{
+    system("chcp 65001  > nul");
+    using namespace std;
+    Stack st;                    // tworzy pusty stos
+    char ch;
+    unsigned long po;
+    cout << "Naciśnij D, aby wprowadzić deklarację, \n"
+        << "P, aby przetworzyć deklarację, lub K, aby zakończyć.\n";
+    while (cin >> ch && toupper(ch) != 'K')
+    {
+        while (cin.get() != '\n')
+            continue;
+        if (!isalpha(ch))
+        {
+            cout << '\a';
+            continue;
+        }
+        switch(ch)
+        {
+            case 'D':
+            case 'd': cout << "Podaj numer nowej deklaracji: ";
+                      cin >> po;
+                      if (st.isfull())
+                          cout << "Stos pełen!\n";
+                      else
+                          st.push(po);
+                      break;
+            case 'P':
+            case 'p': if (st.isempty())
+                          cout << "Stos pusty!\n";
+                      else {
+                          st.pop(po);
+                          cout << "Deklaracja nr " << po << " zdjęta\n";
+                      }
+                      break;
+        }
+        cout << "Naciśnij D, aby wprowadzić deklarację, \n"
+            << "P, aby przetworzyć deklarację, lub K, aby zakończyć.\n";
+    }
+    cout << "Kopia utworzona!\n";
+    Stack clone=st;
+        cout << "Naciśnij D, aby wprowadzić deklarację, \n"
+        << "P, aby przetworzyć deklarację, lub K, aby zakończyć.\n";
+    while (cin >> ch && toupper(ch) != 'K')
+    {
+        while (cin.get() != '\n')
+            continue;
+        if (!isalpha(ch))
+        {
+            cout << '\a';
+            continue;
+        }
+        switch(ch)
+        {
+            case 'D':
+            case 'd': cout << "Podaj numer nowej deklaracji: ";
+                      cin >> po;
+                      if (clone.isfull())
+                          cout << "Stos pełen!\n";
+                      else
+                          clone.push(po);
+                      break;
+            case 'P':
+            case 'p': if (clone.isempty())
+                          cout << "Stos pusty!\n";
+                      else {
+                          clone.pop(po);
+                          cout << "Deklaracja nr " << po << " zdjęta\n";
+                      }
+                      break;
+        }
+        cout << "Naciśnij D, aby wprowadzić deklarację, \n"
+            << "P, aby przetworzyć deklarację, lub K, aby zakończyć.\n";
+    }
+    cout << "Fajrant!\n";
+    return 0;
+}
+
+
