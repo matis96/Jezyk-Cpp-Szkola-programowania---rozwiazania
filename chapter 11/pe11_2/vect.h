9165bc7fbb8053a1400e671906ea121d4084435f
Matis <mateuszgrzyb96@gmail.com>
2024-07-08 17:20:51 +0200

    Add files via upload

---
+++ chapter 11/pe11_2/vect.h
@@ -0,0 +1,49 @@
+// vect.h -- klasa Vector z operatorem << i prze³¹cznikiem trybu
+#ifndef VECTOR_H_
+#define VECTOR_H_
+#include <iostream>
+#include <cmath>
+using std::sqrt;
+namespace VECTOR
+{
+    class Vector
+    {
+        public:
+            enum Mode {RECT, POL};
+            // RECT to wspó³rzêdne kartezjañskie, POL to wspó³rzêdne biegunowe
+        private:
+            double x;        // wartoœæ sk³adowej poziomej
+            double y;        // wartoœæ sk³adowej pionowej
+            //double mag;      // d³ugoœæ wektora
+            //double ang;      // kierunek wektora (w stopniach)
+            Mode mode;       // RECT lub POL
+            // prywatne metody ustawiaj¹ce sk³adowe
+            //void set_mag();
+            //void set_ang();
+            void set_x(double, double);
+            void set_y(double, double);
+        public:
+            Vector();
+            Vector(double n1, double n2, Mode form = RECT);
+            void reset(double n1, double n2, Mode form = RECT);
+            ~Vector();
+            double xval() const {return x;}      // zwraca wartoœæ sk³adowej x
+            double yval() const {return y;}      // zwraca wartoœæ sk³adowej y
+            double magval() const {return sqrt(x * x + y * y);}  // zwraca d³ugoœæ
+            double angval() const;   // zwraca k¹t
+            void polar_mode();    // ustawia tryb na biegunowy (POL)
+            void rect_mode();     // ustawia tryb na kartezjañski (RECT)
+            // metody przeci¹¿aj¹ce operatory
+            Vector operator+(const Vector & b) const;
+            Vector operator-(const Vector & b) const;
+            Vector operator-() const;
+            Vector operator*(double n) const;
+            // funkcje zaprzyjaŸnione
+            friend Vector operator*(double n, const Vector & a);
+            friend std::ostream &
+                operator<<(std::ostream & os, const Vector & v);
+    };
+} // koniec przestrzeni nazw VECTOR
+#endif
+
+
