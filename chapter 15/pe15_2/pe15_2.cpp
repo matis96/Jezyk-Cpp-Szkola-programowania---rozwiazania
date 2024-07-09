6a581739cb6584e3397c8f2e64e17a471701add0
Matis <mateuszgrzyb96@gmail.com>
2024-07-09 10:26:09 +0200

    Create pe15_2.cpp

---
+++ chapter 15/pe15_2/pe15_2.cpp
@@ -0,0 +1,63 @@
+// error4.cpp -- używa klas wyjątków
+#include <iostream>
+#include <cmath> // lub math.h; użytkownicy systemu UNIX mogą potrzebować opcji -lm
+#include <stdexcept>
+#include "exc_mean.h"
+// prototypy funkcji
+double hmean(double a, double b);
+double gmean(double a, double b);
+int main()
+{
+system("chcp 65001  > nul");
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
+    double x, y, z;
+
+    cout << "Podaj dwie liczby: ";
+    while (cin >> x >> y)
+    {
+        try {      // początek bloku try
+            z = hmean(x, y);
+            cout << "Średnia harmoniczna liczb " << x << " i " << y
+                << " wynosi " << z << endl;
+            z = gmean(x,y);
+            cout << "Średnia geometryczna liczb " << x << " i " << y
+                << " wynosi " << z << endl;
+            cout << "Podaj kolejną parę liczb <w, aby wyjść>: ";
+        }   // koniec bloku try
+        catch (bad_hmean & bg)   // początek bloku catch
+        {
+            cout<<bg.what();
+            cout << "Spróbuj ponownie.\n";
+            continue;
+        }
+        catch (bad_gmean & hg)
+        {
+            cout << hg.what();
+            cout << "Użyte wartości: " << x << ", "
+                << y << endl;
+            cout << "Niestety, to koniec zabawy.\n";
+            break;
+        } // koniec bloku catch
+    }
+    cout << "Koniec\n";
+    return 0;
+}
+
+double hmean(double a, double b)
+{
+    if (a == -b)
+        throw bad_hmean();
+    return 2.0 * a * b / (a + b);
+}
+
+double gmean(double a, double b)
+{
+    if (a < 0 || b < 0)
+        throw bad_gmean();
+    return std::sqrt(a * b);
+}
+
+
